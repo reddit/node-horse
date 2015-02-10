950adcaa35616d0004eb0f9d76decd937982e707
Jack Lawson <ajacksified@gmail.com>
2015-02-10 12:01:01 -0800

    Node clustering; v0.3.6

--- index.js
+++ index.js
@@ -9,4 +9,52 @@ require('6to5/register')({
 });
 
 // Require in the express server.
-require('./src/server');
+var Server = require('./src/server');
+
+var cluster = require('cluster');
+var numCPUs = require('os').cpus().length;
+
+// App config
+var config = require('./src/config');
+
+// Import built-asset manifests for passing to layouts
+var jsManifest = require('./build/js/client-manifest.json');
+var cssManifest = require('./build/css/css-manifest.json');
+var servers = [];
+
+// Then merge them into a single object for ease of use later
+config.manifest = {};
+Object.assign(config.manifest, jsManifest, cssManifest);
+
+function start(config) {
+  var server = new Server(config);
+  server.start();
+  return server;
+}
+
+// Private, server-only config that we don't put in config.js, which is shared
+config.liveReload = process.env.LIVERELOAD || true;
+config.cookieSecret = process.env.SWITCHAROO_COOKIE_SECRET || 'snoo';
+config.oauth = {
+  clientId: process.env.OAUTH_CLIENT_ID || '',
+  secret: process.env.OAUTH_SECRET || '',
+};
+
+if (cluster.isMaster) {
+  for (var i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', function(worker, code, signal) {
+    if (failedProcesses < 20) {
+      console.log('Worker ' + worker.process.pid + ' died, restarting.');
+      cluster.fork();
+      failedProcesses++;
+    } else {
+      console.log('Workers died too many times, exiting.');
+      process.exit();
+    }
+  });
+} else {
+  servers.push(start(config));
+}
--- src/server.jsx
+++ src/server.jsx
@@ -24,185 +24,192 @@ import plugins from './plugins';
 import App from './app';
 import oauth from './oauth';
 
-// App config
-import config from './config';
-// Import built-asset manifests for passing to layouts
-import jsManifest from '../build/js/client-manifest.json';
-import cssManifest from '../build/css/css-manifest.json';
+class Server {
+  constructor (config) {
+    // Bind all the things
+    this.render = this.render.bind(this);
+    this.error = this.error.bind(this);
 
-// Then merge them into a single object for ease of use later
-config.manifest = {};
-Object.assign(config.manifest, jsManifest, cssManifest);
+    // Intantiate a new App instance (React middleware)
+    var app = new App(config);
+    var server = express();
+    var router = express.Router();
 
-// Private, server-only config that we don't put in config.js, which is shared
-config.liveReload = process.env.LIVERELOAD || true;
-config.cookieSecret = process.env.SWITCHAROO_COOKIE_SECRET || 'snoo';
-config.oauth = {
-  clientId: process.env.OAUTH_CLIENT_ID || '',
-  secret: process.env.OAUTH_SECRET || '',
-};
+    // Set the port that the webserver should run on
+    server.set('port', app.config.port);
 
-// Intantiate a new App instance (React middleware)
-var app = new App(config);
+    // Configure the webserver, and set up middleware
+    server.use(bodyParser.json());
+    server.use(bodyParser.urlencoded({
+      extended: true,
+    }));
 
-// Register the plugins - some plugins mutate other react components, so they
-// need an instance of our App passed in
-var plugin, p;
+    server.use(compression());
+    server.use(cookieParser());
 
-if (plugins) {
-  for (p in plugins) {
-    plugin = plugins[p];
-    plugin.register(app);
-  }
-}
+    server.use(session({
+      secret: app.config.cookieSecret,
+      resave: true,
+      saveUninitialized: true,
+      httpOnly: true,
+      cookie: {
+        maxAge: 60000
+      },
+      rolling: true,
+    }));
 
-// Instantiate the express web server instance
-var server = express();
+    server.use(csurf());
 
-// Instantiate a new express router
-var router = express.Router();
+    server.use(favicon(__dirname + '/../public/favicon.ico'));
 
-// Set the port that the webserver should run on
-server.set('port', app.config.port);
+    // Set up static routes for built (and unbuilt, static) files
+    router.use(express.static(__dirname + '/../build'));
+    router.use(express.static(__dirname + '/../public'));
+    router.use(express.static(__dirname + '/../lib/snooboots/dist'));
 
-// Configure the webserver, and set up middleware
-server.use(bodyParser.json());
-server.use(bodyParser.urlencoded({
-  extended: true,
-}));
+    // Set up oauth routes
+    oauth(app, router);
 
-server.use(compression());
-server.use(cookieParser());
+    var error = this.error;
+    var render = this.render;
 
-server.use(session({
-  secret: config.cookieSecret,
-  resave: true,
-  saveUninitialized: true,
-  httpOnly: true,
-  cookie: {
-    maxAge: 60000
-  },
-  rolling: true,
-}));
+    // Set up the router to listen to ALL requests not caught by the static
+    // directive aboves, and send them into the `App` React middleware instance.
+    router.use(function(req, res, next) {
+      // Gather all data before rendering.
+      req.renderSynchronous = true;
 
-server.use(csurf());
+      // Only cache unauthed requests
+      req.useCache = !(req.session && req.session.token);
 
-server.use(favicon(__dirname + '/../public/favicon.ico'));
+      // Make a copy of response that can be sent into the polymorphic `app`.
+      // Overwrite send with our server's send.
+      var response = res;
 
-// Set up static routes for built (and unbuilt, static) files
-router.use(express.static(__dirname + '/../build'));
-router.use(express.static(__dirname + '/../public'));
-router.use(express.static(__dirname + '/../lib/snooboots/dist'));
+      res.render = function(response) {
+        render(response, req, res, app);
+      };
 
-// Set up oauth routes
-oauth(app, router);
+      res.error = function(response, req, res, app) {
+        error(response, req, res, app);
+      };
 
-// Set up the ability to inject bootstrapping data, so the client doesn't have
-// to re-hit the server for initialization data
-function injectBootstrap(body, props) {
-  var bodyIndex = body.lastIndexOf('</body>');
-  var template = '<script>var bootstrap=' + JSON.stringify(props) + '</script>';
-  return body.slice(0, bodyIndex) + template + body.slice(bodyIndex);
-}
+      req.csrf = req.csrfToken();
 
-// The success render function
-function render (response, req, res, app) {
-  var status = response.status || 200;
-  var body = response.body || '';
-  var props = response.props;
-  var Layout = response.layout;
-
-  if (body) {
-    try {
-      // If it's an object, it's probably React.
-      if (React.isValidElement(body)) {
-        if (Layout) {
-          body = <Layout {...props}>{body}</Layout>;
-        }
-
-        body = React.renderToStaticMarkup(body);
-        body = injectBootstrap(body, props);
+      try {
+        app.route(req, response);
+      } catch (e) {
+        // Handle if custom type (like routeerror)
+        error({
+          message: e.message,
+          stack: e.stack || '',
+          error: e.error || e,
+          status: e.status || 500,
+        }, req, res, app);
       }
-    } catch (e) {
-      return error({
-        status: 500,
-        message: e.toString(),
-        error: e,
-      }, req, res, app);
-    }
-  } else {
-    status = 204;
+    });
+
+    // Finally, listen to the router on `/` (all requests).
+    server.use('/', router);
+
+    this.server = server;
+    this.app = app;
+    this.router = router;
   }
 
-  res.status(status).send(body);
-}
+  start () {
+    // Register the plugins - some plugins mutate other react components, so they
+    // need an instance of our App passed in
+    var plugin, p;
 
-function error (response, req, res, app) {
-  var status = response.status || 500;
-  var message = response.message || 'Unkown error';
-
-  var error = response.error;
-
-  if (config.debug && response.error) {
-    if (response.error.fileName) {
-      message += '\n' + response.error.fileName;
+    if (plugins) {
+      for (p in plugins) {
+        plugin = plugins[p];
+        plugin.register(this.app);
+      }
     }
 
-    if (response.error.stack) {
-      message += '\n' + response.error.stack;
-    }
+    // Listen to a port and shout it to the world.
+    this.server.listen(this.app.config.port);
+    console.log('listening on ' + this.app.config.port);
+  }
 
-    console.log(message);
+  // Set up the ability to inject bootstrapping data, so the client doesn't have
+  // to re-hit the server for initialization data
+  injectBootstrap (body, props) {
+    var bodyIndex = body.lastIndexOf('</body>');
+    var template = '<script>var bootstrap=' + JSON.stringify(props) + '</script>';
+    return body.slice(0, bodyIndex) + template + body.slice(bodyIndex);
+  }
 
-    res.status(status).send(message);
-  } else {
-    var reroute = '/' + status;
+  // The success render function
+  render (response, req, res, app) {
+    var status = response.status || 200;
+    var body = response.body || '';
+    var props = response.props;
+    var Layout = response.layout;
 
-    if (req.url !== reroute) {
-      req.status = status;
-      req.url = '/' + status;
+    var error = this.error;
+    var injectBootstrap = this.injectBootstrap;
 
-      return app.route(req, res);
+    if (body) {
+      try {
+        // If it's an object, it's probably React.
+        if (React.isValidElement(body)) {
+          if (Layout) {
+            body = <Layout {...props}>{body}</Layout>;
+          }
+
+          body = React.renderToStaticMarkup(body);
+          body = injectBootstrap(body, props);
+        }
+      } catch (e) {
+        console.log(e);
+        return this.error({
+          status: 500,
+          message: e.toString(),
+          error: e,
+        }, req, res, app);
+      }
     } else {
-      console.log(response);
-      res.status(500).send('Yo dawg, I heard you liked errors, so I errored while rendering your error page');
+      status = 204;
+    }
+
+    res.status(status).send(body);
+  }
+
+  error (response, req, res, app) {
+    var status = response.status || 500;
+    var message = response.message || 'Unkown error';
+
+    var error = response.error;
+
+    if (this.app.config.debug && response.error) {
+      if (response.error.fileName) {
+        message += '\n' + response.error.fileName;
+      }
+
+      if (response.error.stack) {
+        message += '\n' + response.error.stack;
+      }
+
+      console.log(message);
+
+      res.status(status).send(message);
+    } else {
+      var reroute = '/' + status;
+
+      if (req.url !== reroute) {
+        req.status = status;
+        req.url = '/' + status;
+
+        return app.route(req, res);
+      } else {
+        console.log(response);
+        res.status(500).send('Yo dawg, I heard you liked errors, so I errored while rendering your error page');
+      }
     }
   }
 }
 
-// Set up the router to listen to ALL requests not caught by the static
-// directive aboves, and send them into the `App` React middleware instance.
-router.use(function(req, res, next) {
-  // Gather all data before rendering.
-  req.renderSynchronous = true;
-
-  // Only cache unauthed requests
-  req.useCache = !(req.session && req.session.token);
-
-  // Make a copy of response that can be sent into the polymorphic `app`.
-  // Overwrite send with our server's send.
-  var response = res;
-  res.render = function(response) { render(response, req, res, app); };
-  res.error = function(response, req, res, app) { error(response, req, res, app); };
-
-  req.csrf = req.csrfToken();
-
-  try {
-    app.route(req, response);
-  } catch (e) {
-    // Handle if custom type (like routeerror)
-    error({
-      message: e.message,
-      stack: e.stack || '',
-      error: e.error || e,
-      status: e.status || 500,
-    }, req, res, app);
-  }
-});
-
-// Finally, listen to the router on `/` (all requests).
-server.use('/', router);
-
-// Listen to a port and shout it to the world.
-server.listen(config.port);
-console.log('listening on ' + config.port);
+export default Server;
